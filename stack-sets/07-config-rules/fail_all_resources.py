7191d6a9c79dd9f01c532c2da283711c7038e436
Dennis Clark <boomfish@gmail.com>
2021-03-02 15:14:06 +1100

    fail_all_resources Config rule: exclude default security groups

--- stack-sets/07-config-rules/fail_all_resources.py
+++ stack-sets/07-config-rules/fail_all_resources.py
@@ -6,6 +6,7 @@ config = boto3.client('config')
 kms = boto3.client('kms')
 
 CLOUDFORMATION_TYPE = 'AWS::CloudFormation::Stack'
+DB_SECURITYGROUP_TYPES = {'AWS::RDS::DBSecurityGroup','AWS::Redshift::ClusterSecurityGroup'}
 
 
 def handler(event, context):
@@ -13,22 +14,29 @@ def handler(event, context):
     configItem = invoking_event.get('configurationItem') or invoking_event['configurationItemSummary']
     resource_id = configItem['resourceId']
     resource_type = configItem['resourceType']
+    arn = configItem['ARN']
     print("FunctionName: " + context.function_name)
     print("ResourceId: " + resource_id)
     print("Resourcetype: " + resource_type)
+    print("ARN: " + arn)
     compliance_type = 'NON_COMPLIANT'
     annotation = 'No Resources should be deployed in this Region'
     if (
             (context.function_name == resource_id and resource_type == 'AWS::Lambda::Function') or
             (os.environ['StackName'] in resource_id and resource_type == CLOUDFORMATION_TYPE) or
             ('StackSet-' in resource_id and resource_type == CLOUDFORMATION_TYPE) or
-            (resource_type == 'AWS::RDS::DBSecurityGroup' and resource_id == 'default')
+            (resource_id == 'default' and resource_type in DB_SECURITYGROUP_TYPES)
     ):
         compliance_type = 'COMPLIANT'
         annotation = 'Compliant'
     elif resource_type == 'AWS::KMS::Key':
         kms_key_details = kms.describe_key(KeyId=resource_id)
         if kms_key_details['KeyMetadata']['KeyManager'] == 'AWS':
+            compliance_type = 'COMPLIANT'
+            annotation = 'Compliant'
+    elif resource_type == 'AWS::EC2::SecurityGroup':
+        default_security_group_arn = 'arn:aws:ec2:' + configItem['awsRegion'] + ':' + configItem['awsAccountId'] + ':security-group/default'
+        if arn == default_security_group_arn:
             compliance_type = 'COMPLIANT'
             annotation = 'Compliant'
 
